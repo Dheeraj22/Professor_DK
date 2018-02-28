0187f1e59178b61c18cd94a02358780ce7e7d131
dheeraj_kamath <dheerajkamath22@gmail.com>
2018-02-28 08:01:34 +0530

    Timetable E2.2 Code added

---
+++ Time Table App/app/src/main/java/com/example/android/timetabledemo/Day_main.java
@@ -0,0 +1,13 @@
+package com.example.android.timetabledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Day_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_main);
+    }
+}
